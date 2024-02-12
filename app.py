347e3ab683faede837c6fbec31ccebff2cb35678
Scott Lee Chua <scottleechua@gmail.com>
2024-02-12 17:46:27 +0800

    Remove loading screen

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app.title = "spam of the times"
 server = app.server
 
 df = pd.read_csv("text-messages.csv")
-# df = pd.read_csv("https://github.com/scottleechua/data/blob/main/spam-and-other-messages/text-messages.csv?raw=true")
 
 df["date-received"] = pd.to_datetime(df["date-received"]).dt.date
 num_days = (df["date-received"].max() - df["date-received"].min()).days + 1
